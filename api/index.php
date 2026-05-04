e25f019f8180bb12f36dd0fa39f8777dc2588000
Naufal <naufalbhurr@gmail.com>
2026-05-05 00:36:15 +0700

    add storage vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,15 @@
 <?php
 
 require __DIR__ . '/../public/index.php';
+
+$app->useStoragePath($_ENV['APP_STORAGE'] ?? '/tmp/storage');
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
