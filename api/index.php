78751f9092a2740ff85f86a31357bce9e2bd5d92
Naufal <naufalbhurr@gmail.com>
2026-05-05 00:41:09 +0700

    update api index vercel

--- api/index.php
+++ api/index.php
@@ -1,15 +1,9 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $app->useStoragePath($_ENV['APP_STORAGE'] ?? '/tmp/storage');
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+$app->handleRequest(Illuminate\Http\Request::capture());
